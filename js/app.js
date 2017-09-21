1615fe7fe72e883ce976fd40a4b9413444d5f6f9
Andrés Medrano Vizcaíno <andres.medrano@softech.com.ec>
2017-09-21 10:02:02 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,14 +2,14 @@
 var calculadora = {
 	//Creamos las variables del programa
 	pantalla:document.getElementById('display'),
-	guardar:		"0",		//Esta variable guarda el número digitado
-	num1:				" ",		//Operación1
-	num2: 			" ",		//Operación2
-	iniciar:			1,		//inicia un nuevo operador
-	decimal:			0,		//variable para decimales
-	teclaop:		" ",		//Variable al oprimir una tecla
-	operador:		" ",		//Variable operacion matemática
+	guardar:	"0",		//Esta variable guarda el número digitado
+	num1:		" ",		//Operación1
+	num2: 		" ",		//Operación2
+	iniciar:	  1,		//inicia un nuevo operador
+	decimal:	  0,		//variable para decimales
+	teclaop:	" ",		//Variable al oprimir una tecla
+	operador:	" ",		//Variable operacion matemática
 	continuaop:	" ",		//Variable para continuar la operación
 	resultado:	" ",		//Variable que muestra resultado
-	igual:			" ",		//Al presionar igual, nos permitira seguir realizando operaciones 
+	igual:		" ",		//Al presionar igual, nos permitira seguir realizando operaciones 
 }
